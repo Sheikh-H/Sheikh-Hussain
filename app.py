95fe75e5c9d8db012b7642dcd96b3263333a48ec
Sheikh-H <sheikh.hussain1155@gmail.com>
2026-08-18 15:03:25 +0100

    changing the port as render uses port 10000 for public urls to work

--- app.py
+++ app.py
@@ -346,7 +346,7 @@ def bad_request(error):
 def server_error(error):
     return render_template("pages/error/500.html"), 500
 
-
+# changed to port 10000 from 5000 as render uses this for public url to work
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(debug=False, host="0.0.0.0", port=port)
